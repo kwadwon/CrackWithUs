212bc0cc1f2bb5fc857f1882bd00d1c8ba040cdf
Perry Ogwuche <perryogwuche1@gmail.com>
2013-08-21 10:15:32 -0700

    moved wsgi file to base dir

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'CrackItDemo.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'CrackItDemo.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
